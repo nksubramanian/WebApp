14a6f881fc3d15faab03ff2fafcb4e72c656d990
Subramanian N <subramanian.nk@live.com>
2022-02-18 06:42:02 -0600

    first commit

---
+++ WebApp/WebApp/Controllers/ExceptionController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ExceptionController : ControllerBase
+    {
+
+        [HttpGet("errors")]
+        public async Task<string> MyProfile()
+        {
+            var response = string.Empty;
+            using (var client = new HttpClient())
+            {
+                HttpResponseMessage result = await client.GetAsync("https://localhost:6001/api/users/exception");
+                if (result.IsSuccessStatusCode)
+                {
+                    response = await result.Content.ReadAsStringAsync();
+                }
+            }
+            return response;
+
+
+
+
+
+        }
+
+
+
+
+    }
+}
